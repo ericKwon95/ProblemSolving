99564dca5e0344771f89ed57f23e9f19771389b9
Eric Kwon / 권승용 <ericyong95@gmail.com>
2025-03-20 14:47:38 +0900

    Initial commit

---
+++ scripts/leetcode_helper.bash
@@ -0,0 +1,75 @@
+#!/usr/bin/env bash
+
+# Extract the title-slug from the LeetCode URL
+function extract_problem_name() {
+  local url="$1"
+  echo "$url" | sed -n 's/.*\/problems\/\([^/]*\)\/.*/\1/p'
+}
+
+# Create the Swift code snippet
+function create_swift_code_snippet() {
+  local json_data="$1"
+  local question_id="$2"
+  local swift_code
+  swift_code=$(echo -E "$json_data" | jq -r '.data.question.codeSnippets[] | select(.lang == "Swift") | .code')
+  swift_code=$(echo "$swift_code" | sed '1d;$d')
+  echo "$swift_code"
+}
+
+# GraphQL query to fetch problem details
+function make_query() {
+  local question_slug=$1
+  local query='{
+  "query": "query selectProblem($titleSlug: String!) { question(titleSlug: $titleSlug) { questionFrontendId title titleSlug difficulty exampleTestcases codeSnippets { lang langSlug code } } }",
+  "variables": {
+    "titleSlug": "'"$question_slug"'"
+  }
+}'
+  echo "$query"
+}
+
+# Send a POST request to the LeetCode GraphQL API
+function request() {
+  local query="$1"
+  local response
+  response=$(curl -s -X POST -H "Content-Type: application/json" --data "$query" https://leetcode.com/graphql)
+  echo -E "$response"
+}
+
+# Create a Swift file for the LeetCode problem
+function create_swift_file() {
+  local json_data="$1"
+  local question_id
+  local difficulty
+  local title
+  local title_slug
+  local swift_code
+  local file_name
+  local content
+  local unit_test_content
+
+  question_id=$(echo -E "$json_data" | jq -r '.data.question.questionFrontendId')
+  difficulty=$(echo -E "$json_data" | jq -r '.data.question.difficulty')
+  title=$(echo -E "$json_data" | jq -r '.data.question.title')
+  title_slug=$(echo -E "$json_data" | jq -r '.data.question.titleSlug')
+
+  # Generate the code snippet
+  swift_code=$(create_swift_code_snippet "$json_data" "$question_id")
+  file_name="${question_id}. ${title}"
+
+  # Generate the entire Swift code
+  content=$(make_solution_code "$question_id" "$title" "https://leetcode.com/problems/$title_slug/description/" "LeetCode" "$swift_code")
+  unit_test_content=$(make_unit_test_code "$question_id" "LeetCode")
+
+  # Save the files
+  save_swift_file "$file_name" "LeetCode" "$XCODE_MAIN_FOLDER" "$content" "$difficulty"
+  if [ -n "$XCODE_UNIT_TEST_FOLDER" ]; then
+    save_swift_file "LeetCode${question_id}Tests" "LeetCode" "$XCODE_UNIT_TEST_FOLDER" "$unit_test_content"
+  fi
+
+  # Link to the xcodeproj
+  add_to_xcode_project "$XCODE_MAIN_FOLDER" "$file_name" "LeetCode" "$difficulty"
+  if [ -n "$XCODE_UNIT_TEST_FOLDER" ]; then
+    add_to_xcode_project "$XCODE_UNIT_TEST_FOLDER" "LeetCode${question_id}Tests" "LeetCode"
+  fi
+}
---
+++ scripts/programmers_helper.bash
@@ -0,0 +1,68 @@
+#!/usr/bin/env bash
+
+# Extract the problem name from the Programmers URL
+function extract_problem_number() {
+  local problem_info=$1
+  if [[ $problem_info =~ ^[0-9]+$ ]]; then
+    echo "$problem_info"
+  elif [[ $problem_info =~ ^https://school.programmers.co.kr/learn/courses/30/lessons/[0-9]+ ]]; then
+    echo "$problem_info" | sed -n "s/^.*https:\/\/school\.programmers\.co\.kr\/learn\/courses\/30\/lessons\/\([0-9]*\).*$/\1/p"
+  else
+    echo "Problem number or link is required!" >&2
+    return 1
+  fi
+}
+
+# Create the Swift code snippet
+function create_swift_code_snippet() {
+  local problem_link=$1
+  local problem_info
+  local problem_name
+  local swift_code
+  problem_info=$(curl -s -N "$problem_link")
+  problem_name=$(echo "$problem_info" | sed -n "s/^.*<title>코딩테스트 연습 - \(.*\) \| 프로그래머스 스쿨<\/title>.*$/\1/p")
+  swift_code=$(echo "$problem_info" | sed -n '/<textarea hidden id="code" name="code">/,/<\/textarea>/p' | sed 's/<textarea hidden id="code" name="code">//;s/<\/textarea>//')
+  swift_code=$(echo "$swift_code" | sed 's/&gt;/>/g;s/&lt;/</g;s/&quot;/"/g;s/&#39;/'"'"'/g;s/&amp;/\&/g')
+  swift_code=$(echo "$swift_code" | sed '/import Foundation/d')
+  swift_code="${swift_code//  /}"
+
+  echo "$problem_name"
+  echo "$swift_code"
+}
+
+# Function to create a solution file for a Programmers problem
+function create_swift_file() {
+  local problem_number=$1
+  local problem_name
+  local swift_code
+  local problem_link="https://school.programmers.co.kr/learn/courses/30/lessons/$problem_number?language=swift"
+  local file_name
+  local content
+  local unit_test_content
+
+  # Generate code snippets and get the problem name
+  IFS=$'\n' read -r -d '' problem_name swift_code < <(create_swift_code_snippet "$problem_link")
+
+  # Translate Korean to English if the API key is provided.
+  if [ -n "$DEEPL_API_KEY_PATH" ] && [ -f "$DEEPL_API_KEY_PATH" ]; then
+    problem_name=$(translate_file_name "$problem_name")
+  fi
+
+  file_name="$problem_number. $problem_name"
+
+  # Generate the entire Swift code
+  content=$(make_solution_code "$problem_number" "$problem_name" "$problem_link" "Programmers" "$swift_code")
+  unit_test_content=$(make_unit_test_code "$problem_number" "Programmers")
+
+  # Save the files
+  save_swift_file "$file_name" "Programmers" "$XCODE_MAIN_FOLDER" "$content"
+  if [ -n "$XCODE_UNIT_TEST_FOLDER" ]; then
+    save_swift_file "Programmers${problem_number}Tests" "Programmers" "$XCODE_UNIT_TEST_FOLDER" "$unit_test_content"
+  fi
+
+  # Link to the xcodeproj
+  add_to_xcode_project "$XCODE_MAIN_FOLDER" "$file_name" "Programmers"
+  if [ -n "$XCODE_UNIT_TEST_FOLDER" ]; then
+    add_to_xcode_project "$XCODE_UNIT_TEST_FOLDER" "Programmers${problem_number}Tests" "Programmers"
+  fi
+}
