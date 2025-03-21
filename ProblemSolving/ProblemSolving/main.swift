4b7374043b280fcca8e93fcd0814d2ea14108d9c
ericKwon95 <ericyong95@gmail.com>
2025-03-21 11:40:56 +0900

    [Feat] 프로젝트 생성 및 env파일 설정

---
+++ ProblemSolving/ProblemSolving/main.swift
@@ -0,0 +1,11 @@
+//
+//  main.swift
+//  ProblemSolving
+//
+//  Created by 권승용 on 3/21/25.
+//
+
+import Foundation
+
+print("Hello, World!")
+
---
+++ ProblemSolving/ProblemSolvingTests/ProblemSolvingTests.swift
@@ -0,0 +1,16 @@
+//
+//  ProblemSolvingTests.swift
+//  ProblemSolvingTests
+//
+//  Created by 권승용 on 3/21/25.
+//
+
+import Testing
+
+struct ProblemSolvingTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
